6f70215ef252f5ff7510f3ded21cecd864437008
Eugene Skepner <github@skepner.eu>
2016-11-24 16:24:25 +0100

    include for pybind11 is in acmacs-base

--- cc/py.cc
+++ cc/py.cc
@@ -1,33 +1,5 @@
-#pragma GCC diagnostic push
-#ifdef __clang__
-#pragma GCC diagnostic ignored "-Wreserved-id-macro" // in Python.h
-#pragma GCC diagnostic ignored "-Wextra-semi"
-#pragma GCC diagnostic ignored "-Wshadow"
-#pragma GCC diagnostic ignored "-Wsign-conversion"
-#pragma GCC diagnostic ignored "-Wmissing-noreturn"
-#pragma GCC diagnostic ignored "-Wold-style-cast"
-#pragma GCC diagnostic ignored "-Wdeprecated"
-#pragma GCC diagnostic ignored "-Wfloat-equal"
-#pragma GCC diagnostic ignored "-Wrange-loop-analysis"
-#pragma GCC diagnostic ignored "-Wexit-time-destructors"
-#pragma GCC diagnostic ignored "-Wundef"
-#pragma GCC diagnostic ignored "-Wdocumentation"
-#pragma GCC diagnostic ignored "-Wcovered-switch-default"
-#endif
-#include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
-#pragma GCC diagnostic pop
-
-namespace py = pybind11;
-
 #include "export.hh"
-
-// ----------------------------------------------------------------------
-
-#ifdef __clang__
-#pragma GCC diagnostic ignored "-Wmissing-prototypes"
-// #pragma GCC diagnostic ignored "-Wexit-time-destructors"
-#endif
+#include "acmacs-base/pybind11.hh"
 
 // ----------------------------------------------------------------------
 
